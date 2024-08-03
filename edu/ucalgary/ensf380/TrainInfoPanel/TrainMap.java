1ebab5247fdc499840ea40fcf5b25f27eba64042
DeboDam <Debojeetdam.business@gmail.com>
2024-08-03 15:38:00 -0600

    Update TrainMap.java

--- edu/ucalgary/ensf380/TrainInfoPanel/TrainMap.java
+++ edu/ucalgary/ensf380/TrainInfoPanel/TrainMap.java
@@ -1,21 +1,80 @@
 package TrainInfoPanel;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
 
 public class TrainMap extends JPanel {
-    private static final int MAP_WIDTH = 600;
-    private static final int MAP_HEIGHT = 400;
+	private int trainId;
+    private static final int MAP_WIDTH = 1500;
+    private static final int MAP_HEIGHT = 700;
+    private BufferedImage backgroundImage;
 
-    public TrainMap() {
+    public TrainMap(int trainId) {
         setPreferredSize(new Dimension(MAP_WIDTH, MAP_HEIGHT));
+        try {
+           this.backgroundImage = ImageIO.read(new File("edu/ucalgary/ensf380/assets/Trains.png"));
+           this.trainId = trainId;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        // Add code to draw the train map here
-        g.drawString("Train Map", 50, 50);
-        // You can use g.drawLine, g.drawRect, etc. to draw the map
+        
+        
+        g.drawImage(backgroundImage, 0, 0, MAP_WIDTH, MAP_HEIGHT, null);
+        double halfMapHeight = MAP_HEIGHT / 2;
+        
+        // Parse all the stations
+        List<Line> lines = Line.getAllLines();
+        String stationCode = "";
+        
+        for (Line line: lines) {
+        	List<Train> trains = line.getTrains();        	
+        	for (Station station: line.getStations()) {
+        		int stationYCoord = (int) (Math.floor(station.getYCoordinate()));
+        		
+        		if (station.getYCoordinate() < 75) {
+        			stationYCoord = (int) (Math.floor(station.getYCoordinate() / 1.3));
+        		} else if (station.getYCoordinate() < 200) {
+        			stationYCoord = (int) (Math.floor(station.getYCoordinate() / 1.05));
+        		} else if(station.getYCoordinate() > 420) {
+        			stationYCoord = (int) (Math.floor(station.getYCoordinate() * 1.025));
+        		}
+
+        		boolean filled = false;
+        		for (Train train: trains) {
+        			boolean atCurrentStation = (Integer.parseInt(station.getCode().substring(1)) == train.stationNo);
+        			
+        			// If the train is at the current station, then mark it.
+        			if (atCurrentStation) {
+        				if (train.getTrainId() == trainId) {
+        					g.setColor(Color.BLACK);
+                			g.drawOval((int) (Math.floor(station.getXCoordinate() * 1.25)), stationYCoord, 12, 12);
+        					g.setColor(Color.YELLOW);
+        					g.fillOval((int) (Math.floor(station.getXCoordinate() * 1.25)), stationYCoord, 11, 11);
+        				} else {
+        					// Other train colour
+        					g.setColor(Color.BLACK);
+        					g.fillOval((int) (Math.floor(station.getXCoordinate() * 1.25)), stationYCoord, 12, 12);
+        				}
+        				
+        				filled = true;
+        			}
+        		}
+        		
+        		if (!filled) {
+        			g.setColor(Color.BLACK);
+        			g.drawOval((int) (Math.floor(station.getXCoordinate() * 1.25)), stationYCoord, 12, 12);
+        		}
+        	}
+        }
     }
 }
